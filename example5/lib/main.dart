d92334e3c8fe2f698609d0487ea080118d90e93a
Amartya <62711821+amartyaay@users.noreply.github.com>
2022-11-24 00:16:04 +0530

    created overrides for ==, hashcode, toSring

--- example5/lib/main.dart
+++ example5/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:uuid/uuid.dart';
 
 const TextStyle emojiStyle = TextStyle(
   fontSize: 60,
@@ -26,6 +27,35 @@ class MyApp extends StatelessWidget {
       home: const HomePage(),
     );
   }
+}
+
+@immutable
+class Person {
+  final String name;
+  final int age;
+  final String uuid;
+
+  Person({
+    required this.name,
+    required this.age,
+    String? uuid,
+  }) : uuid = uuid ?? const Uuid().v4();
+
+  Person updated([String? name, int? age]) => Person(
+        name: name ?? this.name,
+        age: age ?? this.age,
+        uuid: uuid,
+      );
+  String get displayName => '$name - $age yrs';
+
+  @override
+  bool operator ==(covariant Person other) => uuid == other.uuid;
+
+  @override
+  int get hashCode => uuid.hashCode;
+
+  @override
+  String toString() => 'Person(name: $name,age: $age,uuid: $uuid)';
 }
 
 class HomePage extends ConsumerWidget {
